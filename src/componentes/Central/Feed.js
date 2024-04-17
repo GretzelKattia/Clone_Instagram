d7949c0d6b7f9f3e3b6744456d6acad8bf340b92
Gretzel Kattia <gretzelpenaloza@gmail.com>
2024-04-17 20:47:40 -0300

    atualização Feed

--- src/componentes/Central/Feed.js
+++ src/componentes/Central/Feed.js
@@ -7,12 +7,12 @@ export default function Feed() {
         <div className="Feed">
             <Post
                 title="Post 1"
-                imgSrc="https://via.placeholder.com/150"
+                imgSrc="https://i.pinimg.com/564x/fb/8a/24/fb8a24d408957e55b2431b3cf0f271cb.jpg"
                 body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc." />
 
             <Post
                 title="Post 2"
-                imgSrc="https://via.placeholder.com/150"
+                imgSrc="https://i.pinimg.com/564x/ca/ac/19/caac19cf51d671fbd2ff7c4b254b23ca.jpg"
                 body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc." />
         </div>
     )
