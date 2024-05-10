d3e490d282aa9c31ca96b53dd862e440774c8737
Gretzel Kattia <gretzelpenaloza@gmail.com>
2024-05-10 17:47:19 -0300

    lado esquerdo

--- src/componentes/Esquerda/ConteudoLateralEsquerda.js
+++ src/componentes/Esquerda/ConteudoLateralEsquerda.js
@@ -15,10 +15,6 @@ import { BsPlusSquare } from "react-icons/bs";
 import { MdPerson3 } from "react-icons/md";
 
 
-
-
-
-
 export default function ConteudoLateralEsquerda() {
     return (
         <div className='conteudoLateralEsquerda'>
@@ -29,18 +25,17 @@ export default function ConteudoLateralEsquerda() {
 
             <ul>
                 <strong>
-                <Menu nome="Home" icone={<GoHomeFill fontSize={30}/>} />
+                <Menu nome="Home" icone={<GoHomeFill fontSize={30}/>}/>
                 </strong>
                 <Menu nome="Pesquisar" icone= {<FaMagnifyingGlass fontSize={25}/>}/>
                 <Menu nome="Explorar" icone= {<MdOutlineExplore fontSize={30}/>}/>
-
                 <Menu nome="Mensagens" icone= {<RiMessengerLine fontSize={30}/>}/>
                 <Menu nome="Notificação" icone= {<TiHeartOutline fontSize={30}/>}/>
                 <Menu nome="Criar" icone= {<BsPlusSquare fontSize={25}/>}/>
                 <Menu nome="Reels" icone= { <PiVideoFill fontSize={30}/>}/>
 
-                <div className='icone_perfil'>
-            <Menu nome="Perfil" icone= {<MdPerson3 fontSize={30}/>}/>
+        <div className='icone_perfil'>
+                <Menu nome="Perfil" icone= {<MdPerson3 fontSize={30}/>} style={{ transition: 'transform 0.3s ease-in-out', cursor: 'pointer' }}/>
             
             </div>
 
