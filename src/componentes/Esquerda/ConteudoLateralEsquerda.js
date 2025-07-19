f317fd4b18367883a917b2f6be9008e706d44e8a
Gretzel <gretzelpenaloza@gmail.com>
2025-07-19 18:00:35 -0300

    ajustes: botões responsividade/ fundo storys

--- src/componentes/Esquerda/ConteudoLateralEsquerda.js
+++ src/componentes/Esquerda/ConteudoLateralEsquerda.js
@@ -14,31 +14,29 @@ import { PiVideoFill } from "react-icons/pi";
 import { BsPlusSquare } from "react-icons/bs";
 import { MdPerson3 } from "react-icons/md";
 
-
 export default function ConteudoLateralEsquerda() {
     return (
         <div className='conteudoLateralEsquerda'>
             <img className='imagemInstagram' src={logo} alt='Logo' />
             <ul>
                 <strong>
-                <Menu nome="Home" icone={<GoHomeFill fontSize={30}/>}/>
+                    <Menu nome="Home" icone={<GoHomeFill fontSize={30}/>}/>
                 </strong>
-                <Menu nome="Pesquisar" icone= {<FaMagnifyingGlass fontSize={25}/>}/>
-                <Menu nome="Explorar" icone= {<MdOutlineExplore fontSize={30}/>}/>
-                <Menu nome="Reels" icone= { <PiVideoFill fontSize={30}/>}/>
-                <Menu nome="Mensagens" icone= {<RiMessengerLine fontSize={30}/>}/>
-                <Menu nome="Notificação" icone= {<TiHeartOutline fontSize={30}/>}/>
-                <Menu nome="Criar" icone= {<BsPlusSquare fontSize={25}/>}/>
-
-        <div className='icone_perfil'>
-                <Menu nome="Perfil" icone= {<MdPerson3 fontSize={30}/>} style={{ transition: 'transform 0.3s ease-in-out', cursor: 'pointer' }}/>
-            
-            </div>
-
+                <Menu nome="Pesquisar" icone={<FaMagnifyingGlass fontSize={25}/>}/>
+                <Menu nome="Reels" icone={<PiVideoFill fontSize={30}/>}/>
+                <Menu nome="Mensagens" icone={<RiMessengerLine fontSize={30}/>}/>
+                
+                <div className="desktop-only">
+                    <Menu nome="Explorar" icone={<MdOutlineExplore fontSize={30}/>}/>
+                    <Menu nome="Notificação" icone={<TiHeartOutline fontSize={30}/>}/>
+                    <Menu nome="Criar" icone={<BsPlusSquare fontSize={25}/>}/>
+                </div>
+                
+                <div className='icone_perfil'>
+                    <Menu nome="Perfil" icone={<MdPerson3 fontSize={30}/>} 
+                          style={{ transition: 'transform 0.3s ease-in-out', cursor: 'pointer' }}/>
+                </div>
             </ul>
-
         </div>
     )
 }
-
-// export default ConteudoLateralEsquerda;
